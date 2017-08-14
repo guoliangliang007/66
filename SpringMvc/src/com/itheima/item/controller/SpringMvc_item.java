ae8e4802b9cf4a14a6fa6511985f91ecf49e09cc
gll <1136941477@qq.com>
2017-08-14 21:45:20 +0800

    代码提交

---
+++ SpringMvc/src/com/itheima/item/controller/SpringMvc_item.java
@@ -0,0 +1,31 @@
+package com.itheima.item.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.itheima.pojo.Item;
+
+@Controller
+public class SpringMvc_item {
+    
+	@RequestMapping(value="/findAll.action")
+	public ModelAndView fingAll(){
+		List<Item> list = new ArrayList<Item>();
+		list.add(new Item(1, "1华为 荣耀8", 2399f, new Date(), "质量好！1"));
+		list.add(new Item(2, "2华为 荣耀8", 2399f, new Date(), "质量好！2"));
+		list.add(new Item(3, "3华为 荣耀8", 2399f, new Date(), "质量好！3"));
+		list.add(new Item(4, "4华为 荣耀8", 2399f, new Date(), "质量好！4"));
+		list.add(new Item(5, "5华为 荣耀8", 2399f, new Date(), "质量好！5"));
+		list.add(new Item(6, "6华为 荣耀8", 2399f, new Date(), "质量好！6"));
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("itemList", list);
+		mav.setViewName("/WEB-INF/jsp/itemList.jsp");
+		return mav;
+	}
+	
+}
